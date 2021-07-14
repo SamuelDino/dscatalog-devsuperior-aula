b433070356eb2f198239aa5d06721f6be6ca71e1
samueldino <samuel_qualitats@hotmail.com>
2021-07-14 16:43:55 -0300

    Banco de dados H2 e camadas

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,10 +1,8 @@
 package com.devsuperior.dscatalog.resources;
 
-
-
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.RequestScope;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 
 
@@ -19,11 +18,16 @@ import com.devsuperior.dscatalog.entities.Category;
 @RequestMapping (value = "/categories")
 public class CategoryResource {
 	
+	@Autowired
+	private CategoryService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Computers"));
-		list.add(new Category(2L, "Printers"));
-		return ResponseEntity.ok(list);
+		//List<Category> list = new ArrayList<>();
+		//list.add(new Category(1L, "Computers"));
+		//list.add(new Category(2L, "Printers"));
+		//return ResponseEntity.ok(list);
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok(list);		
 	}
 }
