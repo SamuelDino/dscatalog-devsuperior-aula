2db910fc2fb77c0463d035c076d9efd2ba6b496b
samueldino <samuel_qualitats@hotmail.com>
2021-07-14 23:08:18 -0300

    DTO

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -7,9 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
-import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 
@@ -22,12 +21,7 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		//List<Category> list = new ArrayList<>();
-		//list.add(new Category(1L, "Computers"));
-		//list.add(new Category(2L, "Printers"));
-		//return ResponseEntity.ok(list);
-		List<Category> list = service.findAll();
-		return ResponseEntity.ok(list);		
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		return ResponseEntity.ok(service.findAll());		
 	}
 }
