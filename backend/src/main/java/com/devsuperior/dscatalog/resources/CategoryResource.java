1cf3f0799324e2b7875e84970f5f533745bda462
samueldino <samuel_qualitats@hotmail.com>
2021-07-15 21:41:44 -0300

    Find by Id and exception handler

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class CategoryResource {
 	@GetMapping
 	public ResponseEntity<List<CategoryDTO>> findAll(){
 		return ResponseEntity.ok(service.findAll());		
+	}
+	
+	@GetMapping (value = "/{id}")
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		return ResponseEntity.ok(service.findById(id));		
 	}
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,4 +23,12 @@ public class CategoryService {
 	public List<CategoryDTO> findAll(){	
 		return repository.findAll().stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return new CategoryDTO(obj.orElseThrow(()-> new EntityNotFoundException("Categoria não encontrada !")));
+	}
+
+
 }
